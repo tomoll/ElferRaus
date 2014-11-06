36a3c90cb6e2645dd68aa2ee936c8dabb21d556a
Christian <Christian@172.20.154.71>
2014-11-06 10:14:40 +0100

    playerDeck modified and card entitie created! :-*

--- src/de/htwg/elferraus/entities/playerDeck.java
+++ src/de/htwg/elferraus/entities/playerDeck.java
@@ -1,5 +1,25 @@
 package de.htwg.elferraus.entities;
 
 public class playerDeck {
-
+    private int size;
+    private int[] hans = new int[80];
+    private card[] deck = new card[80];
+    
+    public playerDeck(int size) {
+        this.size = size;
+    }
+    public int getSize(){
+        return this.size;
+    }
+    public void setSize(int size){
+        this.size = size;
+    }
+    public boolean contains (card c){
+        for(int i = 0; i<this.size; i++)    
+            if(c.getColour().equals(deck[i].getColour()) && c.getNumber() == (deck[i].getNumber())) {
+                return true;
+            }
+        return false;
+    }
 }
+
