5bc29c56fc83e949ab58c1d25348c051bc42e94c
Tobias Moll <tomoll@htwg-konstanz.de>
2014-12-04 10:45:29 +0100

    mixCards funktioniert

--- src/main/java/de/htwg/elferraus/entities/MainStack.java
+++ src/main/java/de/htwg/elferraus/entities/MainStack.java
@@ -1,22 +1,24 @@
 package de.htwg.elferraus.entities;
 
+import java.math.BigDecimal;
+import java.util.Random;
+import static java.math.BigDecimal.ZERO;
+
 /**
  *
  * @author Christian Hümmer, Tobias Moll
  */
 public class MainStack {
 
-    
     private int count = 0;
     private String colour = "";
     private final int anzFarben = 4;
     private final int anzKartenproFarbe = 20;
     private final int gesamtKarten = anzFarben * anzKartenproFarbe;
     private Card[] startstack = new Card[gesamtKarten];
-    
+
     //kein Array sondern Stack
     //am besten eigenen Zähler wegen Array 0 quasi 1!!!
-
     public MainStack() {
         for (int j = 0; j < anzFarben; j++) {
             switch (j) {
@@ -55,20 +57,15 @@ public class MainStack {
     }
 
     public void mixCards() {
-        Card[] temp = startstack;
-
-        //for (int i = gesamtKarten; i > 0;) {
-          //  int j = randomGenerator(0, i);
-            //if(startstack[j]==null){
-              //  startstack[j] = temp[i];
-                //i--;
-            //}
-
-        //}
+        Random rnd = new Random();
+        for (int i = startstack.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            Card a = startstack[index];
+            startstack[index] = startstack[i];
+            startstack[i] = a;
+        }
     }
-/*
-    private static int randomGenerator(int low, int high) {
-        return (int) (Math.random() * (high - low) + low);
-    }
-*/
+
 }
+
